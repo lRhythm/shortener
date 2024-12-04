84c573fa6e1a18519aae4ad20ae67c01580ff6d7
Калинин Александр Владимирович <kalinin_av@rusklimat.ru>
2024-12-04 21:57:04 +0300

    iter20: -ldflags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,8 +3,21 @@ Package main - HTTP сервис сокращения URL.
 */
 package main
 
-import "github.com/lRhythm/shortener/internal/app"
+import (
+	"fmt"
+
+	"github.com/lRhythm/shortener/internal/app"
+)
+
+const na = "N/A"
+
+// Флаги линковщика.
+var buildVersion, buildDate, buildCommit = na, na, na
 
 func main() {
+	// Пример запуска из корневой директории проекта:
+	// go run -ldflags "-X main.buildVersion=v1.0.1 -X 'main.buildDate=$(date +'%Y/%m/%d %H:%M:%S')'" ./cmd/shortener/main.go
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
+
 	app.Start()
 }
